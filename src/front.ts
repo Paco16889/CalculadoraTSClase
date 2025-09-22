3f35a0c34d0ea32715e981e10943c50af0ad9f6e
Paco16889 <francisco.cobsan@gmail.com>
2025-09-23 00:04:31 +0200

    buenas noches

--- src/front.ts
+++ src/front.ts
@@ -23,55 +23,23 @@ if (pantalla) {
             }
             return;
                 }
-                
-                if(boton.id)
-               // const operaciones = ['suma', 'resta', 'multiplicar', 'dividir'];
-                const operaciones = new Map<number, string> ([
-                    [0, 'suma'],
-                    [1, 'resta'],
-                    [2, 'multiplicar'],
-                    [3, 'dividir']
 
-                ]);
-                for(const [key, value] of operaciones){
-                    if(boton.id === value){
-                        operador = key;
-
-                    }
+                if (boton.id === 'resultado') {
+                    //immprime la repuesta del back
+                } else {
+                // si por alguna razon no hay resultado lanzamos error
                 }
                 
-                switch (operador) {
-                    case 0:
-                        resultado = operandoA + operandoB;
-                    break;
-                    case 1:
-                        resultado = operandoA - operandoB;
-                    break;
-                    case 2:
-                        resultado = operandoA * operandoB;
-                    break;
-                    case 3:
-                        if(operandoB === 0){
-                            console.log('Error: División por cero.');
-                            return;
-                        }
-                        resultado = operandoA / operandoB;
-                    break;
-    
-                    default:
-                        console.log('Operación no válida.');
-                    break;
-    }
+                
+               // const operaciones = ['suma', 'resta', 'multiplicar', 'dividir'];
+              
+                
+                
                 
             });
         });
     }
 }
 
-async (operandoA:number, operandoB: number, operador: string) => {
-    
-}
 
-function imprimeEnPantalla(opera:type) {
-    
-}
+
